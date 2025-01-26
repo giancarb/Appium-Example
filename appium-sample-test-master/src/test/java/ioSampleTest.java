ab1f866208426555e4881c14cceba901203d91be
giancarb <0522500116mM>
2025-01-26 19:20:14 +0100

    update

--- appium-sample-test-master/src/test/java/ioSampleTest.java
+++ appium-sample-test-master/src/test/java/ioSampleTest.java
@@ -1,9 +1,6 @@
-import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import io.appium.java_client.remote.MobileCapabilityType;
+import io.appium.java_client.android.options.UiAutomator2Options;
 import org.openqa.selenium.By;
-import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
@@ -12,40 +9,41 @@ import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.time.Duration;
 
 public class ioSampleTest {
 
-    public AndroidDriver<MobileElement> driver;
+    public AndroidDriver driver;
     public WebDriverWait wait;
 
     //Elements
-    String secondNewJob = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.LinearLayout";
-
-
-
-
+    final String secondNewJob = "/hierarchy/android.widget.FrameLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.widget.FrameLayout/android.support.v4.widget.DrawerLayout/android.widget.LinearLayout/android.widget.FrameLayout/android.widget.LinearLayout/android.view.ViewGroup/android.widget.LinearLayout[1]/android.support.v7.widget.RecyclerView/android.widget.FrameLayout[1]/android.widget.LinearLayout";
 
 
     @BeforeMethod
-    public void setup () throws MalformedURLException {
-        DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability("deviceName", "Pixel 2 API 24");
-        caps.setCapability("udid", "emulator-5554"); //DeviceId from "adb devices" command
-        caps.setCapability("platformName", "Android");
-        caps.setCapability("platformVersion", "7.0");
-        caps.setCapability("skipUnlock","true");
-        caps.setCapability("appPackage", "com.isinolsun.app");
-        caps.setCapability("appActivity","com.isinolsun.app.activities.SplashActivity");
-        caps.setCapability("noReset","false");
+    public void setup() throws MalformedURLException {
 
-        //caps.setCapability(MobileCapabilityType.APP, "/Users/gianlucacarbone/Documents/dev/IdeaProjects/appium-sample-test-master/apk/isinolsun-v2.1.7.apk" );
-        driver = new AndroidDriver<MobileElement>(new URL("http://0.0.0.0:4723/wd/hub"),caps);
-        wait = new WebDriverWait(driver, 10);
+
+        UiAutomator2Options options = new UiAutomator2Options()
+                .setUdid("emulator-5554")  //DeviceId from "adb devices" command
+                .setApp("/Users/gianlucacarbone/Dev/Appium/Appium-Example/appium-sample-test-master/apk/isinolsun-v2.1.7.apk")
+                .setAppActivity("com.isinolsun.app.activities.SplashActivity")
+                .setAppPackage("com.isinolsun.app")
+                .setDeviceName("Pixel 5 API 24")
+                .setPlatformVersion("7.0")
+                .setPlatformName("Android")
+                .setAutomationName("UiAutomator2")
+                .setNoReset(false)
+                .setSkipUnlock(true);
+
+        driver = new AndroidDriver(new URL("http://0.0.0.0:4723"), options);
+
+        wait = new WebDriverWait(driver, Duration.ofSeconds(10));
     }
 
 
     @Test
-    public void basicTest () throws InterruptedException {
+    public void basicTest() {
         //Click and pass Splash
         wait.until(ExpectedConditions.visibilityOfElementLocated
                 (By.id("com.isinolsun.app:id/animation_view"))).click();
@@ -58,7 +56,7 @@ public class ioSampleTest {
         wait.until(ExpectedConditions.visibilityOfElementLocated
                 (By.id("com.android.packageinstaller:id/permission_allow_button")));
 
-        if (driver.findElements(By.id("com.android.packageinstaller:id/permission_allow_button")).size() > 0) {
+        if (!driver.findElements(By.id("com.android.packageinstaller:id/permission_allow_button")).isEmpty()) {
             driver.findElements(By.id("com.android.packageinstaller:id/permission_allow_button")).get(0).click();
         }
 
@@ -68,7 +66,7 @@ public class ioSampleTest {
     }
 
     @AfterMethod
-    public void teardown(){
+    public void teardown() {
         driver.quit();
     }
 }
